31ee94691293b926de77194585714d75c80fea0d
Ben <lolitsaburrito@gmail.com>
2021-11-23 10:45:34 -0800

    improve/cleanup event_handler logic

--- lib/cai_data/event_handler.ex
+++ lib/cai_data/event_handler.ex
@@ -23,18 +23,14 @@ defmodule CAIData.EventHandler do
          %{"character_id" => character_id, "amount" => xp_amount, "experience_id" => xp_id} =
            payload}
       ) do
-    case SessionHandler.get(character_id) do
-      {:ok, %CharacterSession{xp_types: xp_types} = session} ->
-        xp = String.to_integer(xp_amount)
+    with {:ok, %CharacterSession{xp_types: xp_types} = session} <- SessionHandler.get(character_id) do
+      xp = String.to_integer(xp_amount)
 
-        CharacterSession.changeset(session, %{
-          xp_earned: session.xp_earned + xp,
-          xp_types: Map.update(xp_types, xp_id, xp, &(&1 + xp))
-        })
-        |> SessionHandler.update()
-
-      _ ->
-        nil
+      CharacterSession.changeset(session, %{
+        xp_earned: session.xp_earned + xp,
+        xp_types: Map.update(xp_types, xp_id, xp, &(&1 + xp))
+      })
+      |> SessionHandler.update()
     end
 
     if xp_id == "1520" do
@@ -56,36 +52,29 @@ defmodule CAIData.EventHandler do
            "is_headshot" => is_headshot
          }}
       ) do
-    # If the weapon is considered an IvI weapon and the attacker is not in a vehicle, is_ivi_kill = true
-    is_ivi_kill = Enum.member?(CAIData.ivi_weapon_ids(), weapon_id) and vehicle_id == "0"
-    is_headshot = String.to_integer(is_headshot)
+    # If the weapon is considered an IvI weapon and the attacker is not in a vehicle, ivi_kill = 1, else ivi_kill = 0
+    ivi_kill =
+      if Enum.member?(CAIData.ivi_weapon_ids(), weapon_id) and vehicle_id == "0",
+        do: 1,
+        else: 0
+    headshot_kill = String.to_integer(is_headshot)
 
-    case SessionHandler.get(character_id) do
-      {:ok, %CharacterSession{} = session} ->
-        CharacterSession.changeset(session, %{
-          deaths: session.deaths + 1,
-          deaths_ivi: session.deaths_ivi + ((is_ivi_kill == true && 1) || 0)
-        })
-        |> SessionHandler.update()
-
-      _ ->
-        nil
+    with {:ok, %CharacterSession{} = session} <- SessionHandler.get(character_id) do
+      CharacterSession.changeset(session, %{
+        deaths: session.deaths + 1,
+        deaths_ivi: session.deaths_ivi + ivi_kill
+      })
+      |> SessionHandler.update()
     end
 
-    case SessionHandler.get(attacker_id) do
-      {:ok, %CharacterSession{} = session} ->
-        changes = %{
-          kills: session.kills + 1,
-          kills_hs: session.kills_hs + is_headshot,
-          kills_ivi: session.kills_ivi + 1,
-          kills_hs_ivi: session.kills_hs_ivi + ((is_ivi_kill and is_headshot == 1 && 1) || 0)
-        }
-
-        CharacterSession.changeset(session, changes)
-        |> SessionHandler.update()
-
-      _ ->
-        nil
+    with {:ok, %CharacterSession{} = session} <- SessionHandler.get(attacker_id) do
+      CharacterSession.changeset(session, %{
+        kills: session.kills + 1,
+        kills_hs: session.kills_hs + headshot_kill,
+        kills_ivi: session.kills_ivi + ivi_kill,
+        kills_hs_ivi: session.kills_hs_ivi + (Bitwise.&&&(ivi_kill, headshot_kill))
+      })
+      |> SessionHandler.update()
     end
   end
 
@@ -103,59 +92,44 @@ defmodule CAIData.EventHandler do
         {"VehicleDestroy",
          %{
            "character_id" => character_id,
-           "attacker_character_id" => attacker_id,
-           "attacker_vehicle_id" => attacker_vehicle_id
+           "vehicle_id" => vehicle_id,
+           "attacker_character_id" => attacker_id
          }}
       ) do
-    with {:ok, vehicle} <- Map.fetch(CAIData.vehicle_info(), attacker_vehicle_id) do
-      case SessionHandler.get(character_id) do
-        {:ok, %CharacterSession{vehicles_lost: vehicles_lost} = session} ->
-          CharacterSession.changeset(session, %{
-            vehicles_lost: Map.update(vehicles_lost, vehicle["name"], 1, &(&1 + 1)),
-            nanites_lost: session.nanites_lost + String.to_integer(vehicle["cost"]),
-            vehicle_deaths: session.vehicle_deaths + 1
-          })
-          |> SessionHandler.update()
-
-        _ ->
-          nil
+    with {:ok, vehicle} <- Map.fetch(CAIData.vehicle_info(), vehicle_id) do
+      with {:ok, %CharacterSession{vehicles_lost: vehicles_lost} = session} <- SessionHandler.get(character_id) do
+        CharacterSession.changeset(session, %{
+          vehicles_lost: Map.update(vehicles_lost, vehicle["name"], 1, &(&1 + 1)),
+          nanites_lost: session.nanites_lost + String.to_integer(vehicle["cost"]),
+          vehicle_deaths: session.vehicle_deaths + 1
+        })
+        |> SessionHandler.update()
       end
 
-      case SessionHandler.get(attacker_id) do
-        {:ok, %CharacterSession{vehicles_destroyed: vehicles_destroyed} = session}
-        when attacker_id != character_id ->
-          CharacterSession.changeset(session, %{
-            vehicles_destroyed: Map.update(vehicles_destroyed, vehicle["name"], 1, &(&1 + 1)),
-            nanites_destroyed: session.nanites_destroyed + String.to_integer(vehicle["cost"]),
-            vehicle_kills: session.vehicle_kills + 1
-          })
-          |> SessionHandler.update()
+      with {:ok, %CharacterSession{vehicles_destroyed: vehicles_destroyed} = session}
+           when attacker_id != character_id <- SessionHandler.get(attacker_id) do
 
-        _ ->
-          nil
+        CharacterSession.changeset(session, %{
+          vehicles_destroyed: Map.update(vehicles_destroyed, vehicle["name"], 1, &(&1 + 1)),
+          nanites_destroyed: session.nanites_destroyed + String.to_integer(vehicle["cost"]),
+          vehicle_kills: session.vehicle_kills + 1
+        })
+        |> SessionHandler.update()
       end
     end
   end
 
   def handle_event({"PlayerFacilityCapture", %{"character_id" => character_id}}) do
-    case SessionHandler.get(character_id) do
-      {:ok, %CharacterSession{} = session} ->
-        CharacterSession.changeset(session, %{base_captures: session.base_captures + 1})
-        |> SessionHandler.update()
-
-      _ ->
-        nil
+    with {:ok, %CharacterSession{} = session} <- SessionHandler.get(character_id) do
+      CharacterSession.changeset(session, %{base_captures: session.base_captures + 1})
+      |> SessionHandler.update()
     end
   end
 
   def handle_event({"PlayerFacilityDefend", %{"character_id" => character_id}}) do
-    case SessionHandler.get(character_id) do
-      {:ok, %CharacterSession{} = session} ->
-        CharacterSession.changeset(session, %{base_defends: session.base_defends + 1})
-        |> SessionHandler.update()
-
-      _ ->
-        nil
+    with {:ok, %CharacterSession{} = session} <- SessionHandler.get(character_id) do
+      CharacterSession.changeset(session, %{base_defends: session.base_defends + 1})
+      |> SessionHandler.update()
     end
   end
 
@@ -185,22 +159,26 @@ defmodule CAIData.EventHandler do
   # def handle_event({"SkillAdded", payload}), do: nil
   # def handle_event({"AchievementEarned", payload}), do: nil
   def handle_event({"BattleRankUp", %{"character_id" => character_id, "battle_rank" => br}}) do
-    case SessionHandler.get(character_id) do
-      {:ok, %CharacterSession{} = session} ->
-        CharacterSession.changeset(session, %{br_ups: [br | session.br_ups]})
-        |> SessionHandler.update()
-
-      _ ->
-        nil
+    with {:ok, %CharacterSession{} = session} <- SessionHandler.get(character_id) do
+      CharacterSession.changeset(session, %{br_ups: [br | session.br_ups]})
+      |> SessionHandler.update()
     end
   end
 
-  # def handle_event({"MetagameEvent", %{"metagame_event_id" => metagame_event_id, "metagame_event_state" => "138"}}) do
+  # Metagame end
+  def handle_event({"MetagameEvent", %{"metagame_event_id" => event_id_str, "metagame_event_state" => "138"}}) do
+    event_id = String.to_integer(event_id_str)
+
+    event = {:metagame_end, Map.get(CAIData.event_info(), event_id), []}
+    PubSub.broadcast(:ps2_events, "game_stats", event)
+  end
+
+  # Metagame start
   def handle_event(
         {"MetagameEvent",
-         %{"metagame_event_id" => metagame_event_id, "metagame_event_state" => "135"}}
+         %{"metagame_event_id" => event_id_str, "metagame_event_state" => "135"}}
       ) do
-    event_id = String.to_integer(metagame_event_id)
+    event_id = String.to_integer(event_id_str)
 
     query =
       from(s in "event_subscriptions",
@@ -209,17 +187,18 @@ defmodule CAIData.EventHandler do
       )
 
     user_ids = Repo.all(query)
-    event = {Map.get(CAIData.event_info(), event_id), user_ids}
-    PubSub.broadcast(:ps2_events, "metagame", event)
+    event = {:metagame_start, Map.get(CAIData.event_info(), event_id_str), user_ids}
+    PubSub.broadcast(:ps2_events, "game_status", event)
   end
 
   def handle_event({"ContinentLock", %{"world_id" => world_id, "zone_id" => zone_id}}) do
-    event = {Map.get(CAIData.world_info(), world_id), Map.get(CAIData.zone_info(), zone_id)}
-    PubSub.broadcast(:ps2_events, "zonelock", event)
+    event = {:lock, Map.get(CAIData.world_info(), world_id), Map.get(CAIData.zone_info(), zone_id), []}
+    PubSub.broadcast(:ps2_events, "game_status", event)
   end
 
-  def handle_event({"ContinentUnlock", %{"world_id" => world_id, "zone_id" => zone_id}}) do
-    world_id = String.to_integer(world_id)
+  def handle_event({"ContinentUnlock", %{"world_id" => world_id_str, "zone_id" => zone_id_str}}) do
+    world_id = String.to_integer(world_id_str)
+    zone_id = String.to_integer(zone_id_str)
 
     query =
       from(s in "unlock_subscriptions",
@@ -229,10 +208,8 @@ defmodule CAIData.EventHandler do
 
     user_ids = Repo.all(query)
 
-    event =
-      {Map.get(CAIData.world_info(), world_id), Map.get(CAIData.zone_info(), zone_id), user_ids}
-
-    PubSub.broadcast(:ps2_events, "zoneunlock", event)
+    event = {:unlock, Map.get(CAIData.world_info(), world_id_str), Map.get(CAIData.zone_info(), zone_id_str), user_ids}
+    PubSub.broadcast(:ps2_events, "game_status", event)
   end
 
   # Catch-all callback.
